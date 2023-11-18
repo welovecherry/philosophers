bf237ffa4729024c31dbb188414cf691f483ecb4
Jungmin Hong <jungmiho@c5r2s3.42seoul.kr>
2023-11-18 21:34:42 +0900

    made 2 threads with flags that a philo died

--- only_1_philo.c
+++ only_1_philo.c
@@ -6,7 +6,7 @@
 
 pthread_mutex_t	fork1;
 
-int	counter = 0;
+//int	counter = 0;
 
 // TODO: 철학자 수는 1 이상이기 때문에 -, 음수 될  수 없다.
 int ft_atoi(const char *str)
@@ -47,8 +47,8 @@ typedef struct	s_philo
 	long	time_to_eat;
 	long	time_to_sleep;
 	int		philo_idx;
-	long	beginning_of_simulation;
-	int		*flag_anyone_dead;
+	long	begin_of_simul;
+	int		*flag_anyone_die;
 	int		has_left_fork;
 	int		has_right_fork;
 }	t_philo;
@@ -57,14 +57,14 @@ int	is_this_philo_starve_to_die(long begin_of_last_meal, t_philo *d)
 {
 	long	current_time;
 
-	if (*(d->flag_anyone_dead))
+	if (*(d->flag_anyone_die))
 		return (1);
 	current_time = get_time_in_ms();
 	if (current_time - begin_of_last_meal >= d->time_to_die)
 	{
-		*(d->flag_anyone_dead) = 1;
+		*(d->flag_anyone_die) = 1;
 		current_time = get_time_in_ms();
-		printf("%ld %d died\n", current_time - d->beginning_of_simulation, d->philo_idx);
+		printf("%ld %d died\n", current_time - d->begin_of_simul, d->philo_idx);
 		return (1);
 	}
 	return (0);
@@ -72,63 +72,63 @@ int	is_this_philo_starve_to_die(long begin_of_last_meal, t_philo *d)
 
 void	*print_philo_state(void *philo_info)
 {
-	long	beginning_of_last_meal;
-	long	beginning_of_sleeping;
+	long	begin_of_last_meal;
+	long	begin_of_sleeping;
 	long	current_time;
 	t_philo	*d;
 
 	d = (t_philo *)philo_info;
-	d->beginning_of_simulation = get_time_in_ms();
-	beginning_of_last_meal = d->beginning_of_simulation;
+	d->begin_of_simul = get_time_in_ms();
+	begin_of_last_meal = d->begin_of_simul;
 	while (1)
 	{
-		if(is_this_philo_starve_to_die(beginning_of_last_meal, d))
+		if(is_this_philo_starve_to_die(begin_of_last_meal, d))
 			return (NULL);
 		if (d->has_left_fork == 0)
 		{
+			if (*(d->flag_anyone_die))
+				return (NULL);
 			current_time = get_time_in_ms();
-			if (*(d->flag_anyone_dead))
+			if(is_this_philo_starve_to_die(begin_of_last_meal, d))
 				return (NULL);
-			if(is_this_philo_starve_to_die(beginning_of_last_meal, d))
-				return (NULL);
-			printf("%ld %d has taken aaa fork\n", current_time - d->beginning_of_simulation, d->philo_idx);
+			printf("%ld %d has taken aaa fork\n", current_time - d->begin_of_simul, d->philo_idx);
 			d->has_left_fork = 1;
-		}
+		} // has_left_fork == 1 ㅎㅐ야하는거 아아닌닌가가?
 		if (d->has_right_fork == 0 && d->num_of_philo > 1)
 		{
+			if (d->flag_anyone_die)
+				return (NULL);
+			if(is_this_philo_starve_to_die(begin_of_last_meal, d))
+				return (NULL);
 			current_time = get_time_in_ms();
-			if (d->flag_anyone_dead)
-				return (NULL);
-			if(is_this_philo_starve_to_die(beginning_of_last_meal, d))
-				return (NULL);
-			printf("%ld %d has taken a fork\n", current_time - d->beginning_of_simulation, d->philo_idx);
+			printf("%ld %d has taken a fork\n", current_time - d->begin_of_simul, d->philo_idx);
 			d->has_right_fork = 1;
 		}
 		if (d->has_left_fork && d->has_right_fork)
 		{
-			beginning_of_last_meal = get_time_in_ms();
-			if (d->flag_anyone_dead)
+			if (d->flag_anyone_die)
 				return (NULL);
-			if(is_this_philo_starve_to_die(beginning_of_last_meal, d))
+			if(is_this_philo_starve_to_die(begin_of_last_meal, d))
 				return (NULL);
-			printf("%ld %d is eating\n", beginning_of_last_meal - d->beginning_of_simulation, d->philo_idx);
+			begin_of_last_meal = get_time_in_ms();
+			printf("%ld %d is eating\n", begin_of_last_meal - d->begin_of_simul, d->philo_idx);
 			usleep(1000 * (d->time_to_eat));
 		}
 		else
 			continue;
-		beginning_of_sleeping = get_time_in_ms();
-		if(is_this_philo_starve_to_die(beginning_of_last_meal, d))
+		if(is_this_philo_starve_to_die(begin_of_last_meal, d))
 			return (NULL);
-		printf("%ld %d is sleeping\n", beginning_of_sleeping - d->beginning_of_simulation, d->philo_idx);
+		begin_of_sleeping = get_time_in_ms();
+		printf("%ld %d is sleeping\n", begin_of_sleeping - d->begin_of_simul, d->philo_idx);
 		while (1)
 		{
 			current_time = get_time_in_ms();
-			if (current_time - beginning_of_sleeping >= (d->time_to_sleep))
+			if (current_time - begin_of_sleeping >= (d->time_to_sleep))
 			{
-				current_time = get_time_in_ms();
-				if(is_this_philo_starve_to_die(beginning_of_last_meal, d))
+				if(is_this_philo_starve_to_die(begin_of_last_meal, d))
 					return (NULL);
-				printf("%ld %d is thinking\n", current_time - d->beginning_of_simulation, d->philo_idx);
+				current_time = get_time_in_ms();
+				printf("%ld %d is thinking\n", current_time - d->begin_of_simul, d->philo_idx);
 				break ;
 			}
 			usleep(10);
@@ -145,12 +145,18 @@ int main(int ac, char **av)
 	t_philo		philo_info[2];
 	int			is_anyone_dead;
 	
+	if (ac != 5 && ac != 6)
+	{
+		printf("invalid ac\n");
+		return (0);
+	}
+
 	is_anyone_dead = 0;
 	philo_info[0].num_of_philo = ft_atoi(av[1]);
 	philo_info[0].time_to_die = (long)ft_atoi(av[2]);
 	philo_info[0].time_to_eat = (long)ft_atoi(av[3]);
 	philo_info[0].time_to_sleep = (long)ft_atoi(av[4]);
-	philo_info[0].flag_anyone_dead = &is_anyone_dead;
+	philo_info[0].flag_anyone_die = &is_anyone_dead;
 	philo_info[0].has_left_fork = 0;
 	philo_info[0].has_right_fork = 0;
 
